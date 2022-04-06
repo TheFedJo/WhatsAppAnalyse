8a206cfa25c0884dc4698510f3bf6a8f67c95085
FedJo <52329960+TheFedJo@users.noreply.github.com>
2022-04-06 17:06:20 +0200

    add MainTest for testing Main

---
+++ test/MainTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    WhatsAppMessageParser parser = new WhatsAppMessageParser();
+    final WhatsAppMessage TEST_MESSAGE1 = new WhatsAppMessage(null, null, null,null);
+
+
+
+    @BeforeEach
+    void setup() {
+
+    }
+
+    @Test
+    void testStringToWordsArray() {
+
+
+
+
+    }
+}
