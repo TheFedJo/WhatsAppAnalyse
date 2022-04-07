b526a1e999ff517aa51fc3279683ae925c2d367b
FedJo <52329960+TheFedJo@users.noreply.github.com>
2022-04-07 22:32:03 +0200

    prove Kortom_Tom wrong

--- test/MainTest.java
+++ test/MainTest.java
@@ -1,10 +1,11 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MainTest {
     WhatsAppMessageParser parser = new WhatsAppMessageParser();
-    final WhatsAppMessage TEST_MESSAGE1 = new WhatsAppMessage(null, null, null,null);
-
 
 
     @BeforeEach
@@ -13,7 +14,11 @@ public class MainTest {
     }
 
     @Test
-    void testStringToWordsArray() {
+    void testRound() {
+        assertSame("gaming", "gaming");
+        assertEquals(Main.round(65.3242, 2), 65.32, 0.001);
+        assertEquals(Main.round(65.5151, 2), 65.52, 0.0001);
+
 
 
 
