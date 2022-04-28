371dc65a47b2eb156a390e4074b3f1fbf3fe232e
FedJo <52329960+TheFedJo@users.noreply.github.com>
2022-04-29 00:24:01 +0200

    restructure InputOutput

--- src/InputOutput.java
+++ src/InputOutput.java
@@ -1,40 +1,37 @@
+import org.jfree.chart.ChartUtils;
+import org.jfree.chart.JFreeChart;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class InputOutput {
     private final File inputFile;
     private final File outputDirectory;
     private FileWriter fileWriter;
 
-
     public InputOutput(File inputFile) {
-
         this.inputFile = inputFile;
         String outputPath = inputFile.getParent() + File.separator + inputFile.getName().split("\\.")[0] + "__report";
-        File outputDirectory1 = new File(outputPath);
-
-        try {
-            if (outputDirectory1.listFiles() != null && outputDirectory1.isDirectory()) {
-                    for (File file : outputDirectory1.listFiles()) {
-                        file.delete();
-                    }
-
-            }
-            Files.deleteIfExists(Paths.get(outputPath));
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
+        deleteDirectory(new File(outputPath));
         this.outputDirectory = new File(outputPath);;
         if(!this.outputDirectory.mkdir() && !this.outputDirectory.isDirectory()) {
             System.out.println("File stuff not working");
             System.exit(1);
         }
         setFileName("default");
+    }
+
+    private void deleteDirectory(File directory) {
+        if (directory != null) {
+            File[] fileList = directory.listFiles();
+            if (fileList != null && fileList.length > 0) {
+                for (File file : fileList) {
+                    deleteDirectory(file);
+                }
+            }
+            directory.delete();
+        }
     }
 
     public void println(String output) {
@@ -55,7 +52,14 @@ public class InputOutput {
         writeToFile(output);
     }
 
+    public String getInputFileName() {
+        return inputFile.getName();
+    }
+
     public void setFileName(String fileName) {
+        if (!fileName.endsWith(".txt") && !fileName.endsWith(".md")) {
+            fileName = fileName.split("\\.")[0] + ".txt";
+        }
         if (null != fileWriter) {
             try {
                 fileWriter.close();
@@ -63,10 +67,24 @@ public class InputOutput {
                 e.printStackTrace();
             }
         }
+        String outputPath = outputDirectory.getAbsolutePath() + File.separator + fileName;
         try {
-            Files.deleteIfExists(Path.of(outputDirectory.getAbsolutePath() + File.separator + fileName));
-            File newFile = new File(outputDirectory.getAbsolutePath() + File.separator + fileName);
+            deleteDirectory(new File(outputPath));
+            File newFile = new File(outputPath);
             fileWriter = new FileWriter(newFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void printToPNG(JFreeChart chart, int width, int length) {
+        String fileName = chart.getTitle().getText();
+        if (!fileName.endsWith(".png")) {
+            fileName = fileName.split("\\.")[0] + ".png";
+        }
+        File outputFile = new File(outputDirectory.getAbsolutePath() + File.separator + fileName);
+        try {
+            ChartUtils.saveChartAsPNG(outputFile, chart, width, length);
         } catch (IOException e) {
             e.printStackTrace();
         }
