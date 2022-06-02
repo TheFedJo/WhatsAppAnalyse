6481ec6997f6f80bfe6060953f5cb592062cd01d
FedJo <52329960+TheFedJo@users.noreply.github.com>
2022-06-02 12:47:29 +0200

    update MainTest

--- test/MainTest.java
+++ test/MainTest.java
@@ -16,8 +16,8 @@ public class MainTest {
     @Test
     void testRound() {
         assertSame("gaming", "gaming");
-        assertEquals(Main.round(65.3242, 2), 65.32, 0.001);
-        assertEquals(Main.round(65.5151, 2), 65.52, 0.0001);
+        assertEquals(WordStats.round(65.3242, 2), 65.32, 0.001);
+        assertEquals(WordStats.round(65.5151, 2), 65.52, 0.0001);
 
 
 
