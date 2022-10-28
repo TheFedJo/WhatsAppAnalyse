6764340bf631e4d9ce1d5e11d3ee6fc4007d78b3
FedJo <52329960+TheFedJo@users.noreply.github.com>
2022-10-28 21:45:11 +0200

    add joinmain idea

---
+++ src/join/JoinMain.java
@@ -0,0 +1,10 @@
+package join;
+
+import parse.MessageType;
+import parse.WhatsAppMessage;
+import parse.WhatsAppMessageParser;
+
+public class JoinMain {
+    private WhatsAppMessageParser parser;
+
+}
