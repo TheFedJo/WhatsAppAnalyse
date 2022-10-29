bc6172655eec7eedaed19a4028847a24437d22e8
FedJo <52329960+TheFedJo@users.noreply.github.com>
2022-10-29 19:10:23 +0200

    lay foundation for JOin

---
+++ src/join/JoinChats.java
@@ -0,0 +1,19 @@
+package join;
+
+import parse.WhatsAppChat;
+import parse.WhatsAppMessage;
+
+import java.util.ArrayList;
+
+public class JoinChats {
+    public WhatsAppChat addChat(ArrayList<WhatsAppChat> chats) {
+        return null;
+
+    }
+
+    public ArrayList<WhatsAppChat> sortChats(ArrayList<WhatsAppChat> chats) {
+        return null;
+
+
+    }
+}
