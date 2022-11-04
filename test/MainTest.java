ec672473567b1e257514e24f8775f5f80776268b
FedJo <52329960+TheFedJo@users.noreply.github.com>
2022-11-04 14:54:23 +0100

    update test classes

--- test/MainTest.java
+++ test/MainTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import parse.WhatsAppMessageParser;
-import stats.WordStats;
+import util.Math;
 
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,8 +18,8 @@ public class MainTest {
     @Test
     void testRound() {
         assertSame("gaming", "gaming");
-        assertEquals(WordStats.round(65.3242, 2), 65.32, 0.001);
-        assertEquals(WordStats.round(65.5151, 2), 65.52, 0.0001);
+        assertEquals(Math.round(65.3242, 2), 65.32, 0.001);
+        assertEquals(Math.round(65.5151, 2), 65.52, 0.0001);
 
 
 
